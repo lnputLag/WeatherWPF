6eeda1b894248160bb6591d845b4bbf94cce43f4
lnputLag <kurasovlpack@gmail.com>
2025-07-10 17:55:08 +0300

    Авторизация в приложении

---
+++ AuthWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WeatherWPF.Models;
+
+namespace WeatherWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UserAuth_Click(object sender, RoutedEventArgs e)
+        {
+            string login = UserLoginField.Text.Trim();
+            string password = UserPassField.Password.Trim();
+            if (login.Equals("") || password.Length < 3)
+            {
+                MessageBox.Show("Вы что-то ввели неверно!");
+                return;
+            }
+
+            User authUser = null;
+            using (AppDbContext db = new AppDbContext())
+            {
+                authUser = db.Users.Where(user => user.Login == login && user.Password == Hash(password)).FirstOrDefault();
+            }
+
+            if (authUser == null)
+                MessageBox.Show("Данный пользователь не зарегестрирован");
+            else 
+            {
+                UserLoginField.Text = "";
+                UserPassField.Password = "";
+                UserAuth.Content = "Готово";
+            }
+        }
+
+        private string Hash(string input)
+        {
+            byte[] temp = Encoding.UTF8.GetBytes(input);
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(temp);
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
