3e319aed55e6bf0bd3ac35abb0727565e0370198
lnputLag <kurasovlpack@gmail.com>
2025-07-10 17:38:09 +0300

    Регистрация в приложении

---
+++ AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WeatherWPF.Models;
+
+namespace WeatherWPF
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql("server=localhost;port=3306;username=root;password=root;database=wpfapp");
+        }
+    }
+}
--- RegisterWindow.xaml.cs
+++ RegisterWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WeatherWPF.Models;
 
 namespace WeatherWPF
 {
@@ -19,14 +21,42 @@ namespace WeatherWPF
     /// </summary>
     public partial class RegisterWindow : Window
     {
+        private AppDbContext _db;
         public RegisterWindow()
         {
             InitializeComponent();
+           _db = new AppDbContext();
         }
 
         private void UserRegister_Click(object sender, RoutedEventArgs e)
         {
+            string login = UserLoginField.Text.Trim();
+            string email = UserEmailField.Text.Trim();
+            string password = UserPassField.Password.Trim();
+            if(login.Equals("") || !email.Contains("@") || password.Length < 3)
+            {
+                MessageBox.Show("Вы что-то ввели неверно!");
+                return;
+            }
 
+            User user = new User(login, email, Hash(password));   
+            _db.Users.Add(user);
+            _db.SaveChanges();
+
+            UserLoginField.Text = "";
+            UserEmailField.Text = "";
+            UserPassField.Password = "";
+            UserRegister.Content = "Готово";
+        }
+
+        private string Hash(string input)
+        {
+            byte[] temp = Encoding.UTF8.GetBytes(input);
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(temp);
+                return Convert.ToBase64String(hash);
+            }
         }
     }
 }
